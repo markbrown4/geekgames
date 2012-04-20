f6928b0a8974337bae353c205872ef961401585d
Mark Brown <markbrown4@gmail.com>
2012-04-20 10:20:03 +1000

    first commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'jquery-rails'
+gem 'pg'
+gem 'devise'
+gem 'delayed_job'
+
+group :assets do
+  gem 'compass-rails'
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeekGames::Application.config.secret_token = 'b69de66f04b67dfc2d48da5a669edba75609d4fc1c8c75e0e75f84b6939183fd05e5e059eff9b8c895d11d08ba785cae00c79203ee7ee8d4bab1d3066cdc91a9'
