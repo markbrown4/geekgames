7fc1a2f1b94bc5b7c5cae28da25803982f8c6da8
Mark Brown <markbrown4@gmail.com>
2012-06-30 17:24:37 +1000

    styles

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,6 +1,8 @@
 class RoundsController < ApplicationController
   def summary
     @deal = Deal.todays
+    redirect_to home_path unless @deal.present?
+    
     @round = current_user.best_round
     @rank = @round.rank.ordinalize
   end
